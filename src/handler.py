bf008f3baeb67e75db7013683c63d72d4fe882ac
Aqeel Ahmed <aqeel.ahmad@pkgcare.com>
2024-12-31 19:46:14 +0000

    updated version

--- src/handler.py
+++ src/handler.py
@@ -1,23 +1,21 @@
 """ Example handler file. """
 
 import runpod
-import torch
 import time
 from utils import *
-from diffusers import CogVideoXPipeline,CogVideoXDDIMScheduler
+import torch
+from diffusers import CogVideoXPipeline
 from diffusers.utils import export_to_video
+
 # If your handler runs inference on a model, load the model here.
 # You will want models to be loaded into memory before starting serverless.
 
 try:
     print("========Init text to video pipeline======")
     pipe = CogVideoXPipeline.from_pretrained(
-        "THUDM/CogVideoX-2b",
-        torch_dtype=torch.float16
+        "THUDM/CogVideoX-5b",
+        torch_dtype=torch.bfloat16
     )
-    pipe.scheduler = CogVideoXDDIMScheduler.from_config(pipe.scheduler.config, timestep_spacing="trailing")
-
-    pipe.enable_model_cpu_offload()
 except RuntimeError:
     quit()
 
@@ -27,48 +25,38 @@ def handler(job):
 
     time_start = time.time()
     input_json = job['input']
-    updated_json = update_json(default_json, input_json)
-    mode = updated_json['mode']
-    prompt = updated_json['prompt']
-    guidance_scale = updated_json['guidance_scale']
-    num_inference_steps = updated_json['num_inference_steps']
-    height, width = aspect_ratios[updated_json['aspect_ratio']]
-    max_sequence_length = updated_json['max_sequence_length']
-    negative_prompt = updated_json["negative_prompt"]
-    number_of_frames = updated_json["num_frames"]
-    fps = updated_json['fps']
-    height, width = aspect_ratios[updated_json['aspect_ratio']]
-
-    height = height - (height % 8)
-    width = width - (width % 8)
+    # updated_json = update_json(default_json, input_json)
+    # mode = updated_json['mode']
+    prompt = input_json.get('prompt',None)
+    guidance_scale = input_json.get('guidance_scale',6)
+    num_inference_steps =input_json.get('num_inference_steps',32)
+    # height, width = aspect_ratios[updated_json['aspect_ratio']]
+    # max_sequence_length = updated_json['max_sequence_length']
+    # negative_prompt = updated_json["negative_prompt"]
+    number_of_frames = input_json.get('num_frames',49)
+    # fps = updated_json['fps']
+    # height, width = aspect_ratios[updated_json['aspect_ratio']]
+    #
+    # height = height - (height % 8)
+    # width = width - (width % 8)
 
     # ============== limitation for CogVideoX-2b model =======================
     # fbs should be 8 and max number of frames should be 48
     fps = 8
-    number_of_frames = number_of_frames - (number_of_frames % fps)
-    number_of_frames = min(number_of_frames, 48)
+    # number_of_frames = number_of_frames - (number_of_frames % fps)
+    # number_of_frames = min(number_of_frames, 48)
 
     try:
 
-        prompt_embeds, _ = pipe.encode_prompt(
-            prompt=prompt,
-            do_classifier_free_guidance=True,
-            num_videos_per_prompt=1,
-            max_sequence_length=max_sequence_length,
-            device="cuda",
-
-            dtype=torch.float16,
-        )
-
         video = pipe(
+            prompt=prompt,
+            num_videos_per_prompt=1,
             num_inference_steps=num_inference_steps,
-            guidance_scale=guidance_scale,
-            prompt_embeds=prompt_embeds,
-            negative_prompt=negative_prompt,
             num_frames=number_of_frames,
-            # height=height,
-            # width=width,
+            guidance_scale=guidance_scale,
+            generator=torch.Generator(device="cuda").manual_seed(42),
         ).frames[0]
+
         file_name = "new_out.mp4"
         export_to_video(video, file_name, fps=fps)
 
